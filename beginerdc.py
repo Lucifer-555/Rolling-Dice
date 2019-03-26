39f01984a62ed9fa66f96546ff22ad606395b431
tanmay99200 <46551689+tanmay99200@users.noreply.github.com>
2019-03-26 17:13:43 +0530

    Add files via upload

---
+++ beginerdc.py
@@ -0,0 +1,27 @@
+import random
+import time
+print("rolling dice game")
+print("press enter to roll")
+input()
+player = random.randint(1,6)
+print("Roll the dice"  + 'str(1,6)')
+
+number=random.randint(1,6)
+if number==1:
+        print("[ 1 ]")
+if number==2:
+        print("[ 2 ]")
+if number==3:
+        print("[ 3 ]")
+if number==4:
+        print("[ 4 ]")
+if number==5:
+        print("[ 5 ]")
+if number==6:
+        print("[ 6 ]")
+
+
+
+
+
+
